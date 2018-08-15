f9d35477e14421c0bd9c0ecf782dfc24f354d9a8
Harjot Singh Parmar <hsparmar@uwaterloo.ca>
2018-08-14 22:54:21 -0400

    required Jobs model from db

--- controllers/JobsController.js
+++ controllers/JobsController.js
@@ -1,10 +1,20 @@
 /* TBD: 
-    -   Need to import the models for Jobs here and them display
+-- check if user is logged in 
+    
+Done
+-   Need to import the models for Jobs here and them display -- Done
+    
     
  */
+// import jobs data from databases
+var Jobs = require('../models/Post')
 
 exports.listing = (req,res)=>{
-    res.send(' You have reached the jobs listing page')
+    Jobs.find({},(err,data)=>{
+        console.log('data recieved')
+        res.json(data)
+    })
+    
 }
 
 exports.listing_description = (req,res)=>{
