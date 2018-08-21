a0282bb85809699fe5e40adbcf58402d064ab490
Harjot Singh Parmar <hsparmar@uwaterloo.ca>
2018-08-20 22:37:20 -0400

    changed jobs url to opportunity

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/admin',professorRouter)
 app.use('/studies',studiesRouter)
-app.use('/jobs',jobsRouter)
+app.use('/opportunities',jobsRouter)
 app.use('/labs',labsRouter)
 
 
