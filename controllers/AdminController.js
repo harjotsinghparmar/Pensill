81fc3e5796dee2866c1cee95bc74616ede9f4109
Harjot Singh Parmar <hsparmar@uwaterloo.ca>
2018-10-22 21:16:43 -0400

    Updated admin controller to allow adding studies data/image upload

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -5,6 +5,7 @@
 
  // Admin Page
 // import DB 
+
 var Opportunity = require("../models/Post")
 
 
@@ -16,12 +17,24 @@ var Opportunity = require("../models/Post")
 
  // pages to display opportunities and research studies
  exports.opportunities = (req,res)=>{
+    var mssg = req.query.mssg
+    console.log(mssg)
 Opportunity.find({}).then((data)=>{
-    console.log(data)
-    res.render('admin/opp',{opp: data})
-})
-    
+    res.render('admin/opp',{opp: data,mssg:mssg})
+}) 
 }
+
+// studies listing page
+exports.study = (req,res)=>{
+    var mssg = req.query.mssg
+    console.log(mssg)
+Opportunity.find({}).then((data)=>{
+    res.render('admin/study',{opp: data,mssg:mssg})
+}) 
+}
+
+
+
 
 // Add pages get request
 
@@ -38,22 +51,46 @@ exports.add_job_post = (req,res)=>{
     var opp = new Opportunity({
         title: req.body.title,
         description: req.body.op_description,
-        theme: req.body.op_keywords.split(",")
+        theme: req.body.op_keywords.split(","),
+        requirements: req.body.op_requirements.split(",")
     })
-
+    
     opp.save().then(console.log("done saving"))
 
     res.redirect("/admin/jobs")}
 
 exports.add_study_post = (req,res)=>{
-    res.render('./admin/add_study')
+    console.log("we are in the controller now")
+    console.log(req.body)
+    
+    res.json({file:req.file})
 }
 
 // Edit pages
 
 exports.edit_job = (req,res)=>{
-    res.send(' You have reached the job editing page')
+    var job_id = req.params.id
+    Opportunity.findById(job_id).exec((err,data)=>{
+        console.log(data)
+        res.render('./admin/edit_job',{opportunity:data})
+    })
 }
+exports.edit_job_post = (req,res)=>{
+    var job_id = req.params.id
+    var update = {
+        title: req.body.title,
+        description: req.body.op_description,
+        theme: req.body.op_keywords.split(","),
+        requirements: req.body.op_requirements.split(',')
+    }
+
+    Opportunity.findByIdAndUpdate(job_id,update,{new:true},(err,data)=>{
+        console.log("data has been updated to")
+        var mssg = "Opportunity successfully updated"
+        res.redirect('/admin/jobs/?mssg='+mssg)
+    })
+}
+
 
 exports.edit_study = (req,res)=>{
     res.send(' You have reached study editing page ')
